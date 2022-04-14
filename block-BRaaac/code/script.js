c098a287dc849f52978115119ac1b27c399fd2df
ritik-cyber <ritikdhingra03@gmail.com>
2022-04-14 14:33:55 +0530

    done

---
+++ block-BRaaac/code/script.js
@@ -0,0 +1,81 @@
+let input = document.querySelector('input[type="text"]');
+let rootEle = document.querySelector(".movie-list");
+
+let defaultSelected = "all";
+
+let all = document.querySelector(".all");
+let active = document.querySelector(".active");
+let completed = document.querySelector(".completed ");
+let clearCompleted = document.querySelector(".clear");
+
+let allMOvies = [];
+
+let allMovies = input.addEventListener("keyup", (event) => {
+  if (event.keyCode === 13) {
+    // console.log(event.target.value);
+
+    allMOvies.push({ name: event.target.value, watched: false });
+    event.target.value = "";
+    createLayoutUi();
+  }
+});
+
+function handleChange(event) {
+  let id = event.target.id;
+
+  allMOvies[id].watched = !allMOvies[id].watched;
+}
+
+function elm(type, attr = {}, ...children) {
+  let element = document.createElement(type);
+  for (let key in attr) {
+    if (key.startsWith("data-")) {
+      element.setAttribute(key, attr[key]);
+    } else {
+      element[key] = attr[key];
+    }
+  }
+  children.forEach((child) => {
+    if (typeof child === "object") {
+      element.append(child);
+    }
+    if (typeof child === "string") {
+      let node = document.createTextNode(child);
+      element.append(node);
+    }
+  });
+  return element;
+}
+
+function createLayoutUi(data = allMOvies) {
+  rootEle.innerHTML = "";
+  allMOvies.forEach((movie, i) => {
+    let li = elm(
+      "li",
+      {},
+      elm("input", {
+        type: "checkbox",
+        id: i,
+        checked: movie.watched,
+      }),
+      elm("label", { id: i, innerText: movie.name }),
+      elm("span", { "data-Id": i }, "X")
+    );
+
+    input.addEventListener("change", handleChange);
+
+    span.addEventListener("click", (event) => {
+      let id = event.target.dataset.id;
+      allMOvies.splice(id, 1);
+      createLayoutUi();
+    });
+
+    rootEle.append(li);
+  });
+}
+createLayoutUi();
+
+clearCompleted.addEventListener("click", () => {
+  allMOvies = allMOvies.filter((movie) => !movie.watched);
+  createLayoutUi();
+});
